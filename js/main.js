f856815494ebeeb994867b88cc63e6cc74c0ccbf
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-09-25 22:55:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -54,15 +54,19 @@ $(document).ready(function () {
 $(document).ready(function() {
     setTimeout(showElements, 1200);
     setTimeout(showLogo, 1300);
+    setTimeout(showGlobe, 1300);
 });
 
 function showElements() {
     $(".container").show();
-    $(".intro__globe").show();
 }
 
 function showLogo() {
     $(".header-logo img").show();
+}
+
+function showGlobe() {
+    $(".intro__globe").show();
 }
 
 
