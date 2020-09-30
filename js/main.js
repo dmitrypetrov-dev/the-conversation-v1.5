b90f38fab5b3bac774288cd4baa508041ac8cc9e
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-09-30 20:58:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -62,11 +62,11 @@ function showAll() {
 
 
 $(document).ready(function () {
-    $(".intro__title-1").hide().delay(2800).fadeIn();
-    $(".intro__title-2").hide().delay(2800).fadeIn();
-    $(".intro__title-3").hide().delay(2800).fadeIn();
-    $(".intro__subtitle").hide().delay(5000).fadeIn();
-    $(".intro-btn").hide().delay(5000).fadeIn();
+    $(".intro__title-1").hide().delay(1400).fadeIn();
+    $(".intro__title-2").hide().delay(1400).fadeIn();
+    $(".intro__title-3").hide().delay(1400).fadeIn();
+    $(".intro__subtitle").hide().delay(3000).fadeIn();
+    $(".intro-btn").hide().delay(3000).fadeIn();
 });
 
 
